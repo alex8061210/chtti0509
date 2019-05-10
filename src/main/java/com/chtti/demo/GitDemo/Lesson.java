21fd6c3d2425ec4fe19679cbf18c5846bb84c4e3
AlexWu <alex806121@gmail.com>
2019-05-10 11:12:44 +0800

    Add several endpoint

---
+++ src/main/java/com/chtti/demo/GitDemo/Lesson.java
@@ -0,0 +1,54 @@
+package com.chtti.demo.GitDemo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lesson {
+    private String courseName;
+    private String instructor;
+    private int duration;
+    private List<String> schedule;
+
+    public Lesson(String courseName, String instructor, int duration) {
+        this.courseName = courseName;
+        this.instructor = instructor;
+        this.duration = duration;
+        schedule = new ArrayList<String>();
+    }
+
+    public void addSchedule(String event){
+        schedule.add(event);
+    }
+
+    public List<String> getSchedule() {
+        return schedule;
+    }
+
+    public void setSchedule(List<String> schedule) {
+        this.schedule = schedule;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+
+    public String getInstructor() {
+        return instructor;
+    }
+
+    public void setInstructor(String instructor) {
+        this.instructor = instructor;
+    }
+
+    public int getDuration() {
+        return duration;
+    }
+
+    public void setDuration(int duration) {
+        this.duration = duration;
+    }
+}
--- src/main/java/com/chtti/demo/GitDemo/RootController.java
+++ src/main/java/com/chtti/demo/GitDemo/RootController.java
@@ -10,18 +10,43 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RootController {
     private static final Logger LOGGER = LoggerFactory.getLogger(RootController.class);
+
     //@GetMapping(value = "/")
-    @RequestMapping(value = "/",method = RequestMethod.GET)
-    public String foo(){
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String foo() {
         LOGGER.info("This is pretier comment.");
         //System.out.println("This is ugly comment.");
         return "[Get]Hello spring, check your console.";
     }
 
-    @RequestMapping(value = "/",method = RequestMethod.POST)
-    public String bar(){
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String bar() {
         LOGGER.info("This is pretier comment.");
         //System.out.println("This is ugly comment.");
         return "[Post]Hello spring, check your console.";
     }
+
+    @RequestMapping(value = "/ep1", method = RequestMethod.GET)
+    public String ep1() {
+        return "This is endpoint1";
+    }
+
+    @RequestMapping(value = "/ep2", method = RequestMethod.GET)
+    public String ep2() {
+        return "This is endpoint2";
+    }
+
+    @RequestMapping(value = "/ep3", method = RequestMethod.GET)
+    public Lesson ep3() {
+        Lesson lesson = new Lesson("Java OO", "MarkHo", 28);
+        lesson.addSchedule("Apr-4");
+        lesson.addSchedule("Apr-5");
+        return lesson;
+    }
+
+    @RequestMapping(value = "/ep4", method = RequestMethod.GET)
+    public Lesson ep4() {
+        return new Lesson("R caret", "MarkHo", 35);
+    }
+
 }
